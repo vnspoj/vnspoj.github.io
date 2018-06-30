1ba72c895439c22d386d1da7866b1c5ee22b8093
huynhsamha <huynhsamha@gmail.com>
2018-06-30 10:12:10 +0700

    add button scroll top with animate

--- static/js/main.js
+++ static/js/main.js
@@ -6,4 +6,10 @@
     wheelSpeed: 1.8
   });
 
+  $('#js-scroll-top').click(() => {
+    $('#js-wrapper-body').animate({
+      scrollTop: $('#js-top-element').offset().top
+    }, 500);
+  });
+
 })();
