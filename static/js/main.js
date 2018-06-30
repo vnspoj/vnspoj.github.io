4a06e1e64862a6cd0d679ecef905842387b4cddb
huynhsamha <huynhsamha@gmail.com>
2018-06-30 09:54:58 +0700

    fix body scrollbar

--- static/js/main.js
+++ static/js/main.js
@@ -2,6 +2,8 @@
   const { PerfectScrollbar } = window;
 
   // Awesome scrollbar body
-  const ps = new PerfectScrollbar('#js-wrapper-body');
+  const ps = new PerfectScrollbar('#js-wrapper-body', {
+    wheelSpeed: 1.8
+  });
 
 })();
