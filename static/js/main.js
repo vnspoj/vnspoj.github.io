b39d8c2fba4bf3ca660733049233f73a5c34d265
huynhsamha <huynhsamha@gmail.com>
2018-06-30 10:27:07 +0700

    show/hide button scroll top

--- static/js/main.js
+++ static/js/main.js
@@ -1,14 +1,27 @@
 (() => {
   const { PerfectScrollbar } = window;
 
+  const $scrollTop = $('#js-scroll-top');
+  const $topElement = $('#js-top-element');
+  const $wrapperBody = $('#js-wrapper-body');
+
   // Awesome scrollbar body
   const ps = new PerfectScrollbar('#js-wrapper-body', {
     wheelSpeed: 1.8
   });
 
-  $('#js-scroll-top').click(() => {
-    $('#js-wrapper-body').animate({
-      scrollTop: $('#js-top-element').offset().top
+  $wrapperBody.scroll(function () {
+    const $this = $(this);
+    if ($this.scrollTop() > 800) {
+      $scrollTop.fadeIn();
+    } else {
+      $scrollTop.fadeOut();
+    }
+  });
+
+  $scrollTop.click(() => {
+    $wrapperBody.animate({
+      scrollTop: $topElement.offset().top
     }, 500);
   });
 
