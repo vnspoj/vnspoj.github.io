a48fb495388794c6647c71b1fabbbebf8cf37553
Huỳnh Hà <huynhsamha@gmail.com>
2018-07-08 19:49:53 +0700

    fix issue #8

--- static/js/load-code.js
+++ static/js/load-code.js
@@ -1,33 +1,38 @@
 (() => {
   const { Prism, PerfectScrollbar } = window;
 
-  const filename = $('#js-filename-code').attr('value');
-  let lang = 'cpp';
-  if (filename && filename.split('.').length > 0) {
-    lang = filename.split('.')[1];
-  }
-
-  const ps = new PerfectScrollbar('#js-area-code', {
+  const ps = new PerfectScrollbar('.js-area-code', {
     wheelSpeed: 1.2
   });
 
-  $.get(`https://vnspoj.github.io/solution/src/${filename}`, (code) => {
-    const highlightCode = Prism.highlight(code, Prism.languages[lang]);
-    $('#js-source-code').addClass(`language-${lang}`);
-    $('#js-source-code').html(highlightCode);
+  $('.js-single-src').each(function () {
+    const $src = $(this);
+    const filename = $src.children('.js-filename-code').attr('value');
+    let lang = 'cpp';
+    if (filename && filename.split('.').length > 0) {
+      lang = filename.split('.')[1];
+    }
+
+    $.get(`https://vnspoj.github.io/solution/src/${filename}`, (code) => {
+      const highlightCode = Prism.highlight(code, Prism.languages[lang]);
+      $src.find('.js-source-code')
+        .addClass(`language-${lang}`)
+        .html(highlightCode);
+    });
+
+    $src.find('.js-toggle-code').click(function () {
+      $src.children('.js-area-code').slideToggle(500, 'linear');
+      const $this = $(this);
+      if ($this.attr('data-visible') == '1') {
+        $this.attr('data-visible', '0');
+        $this.children('span.text').text('Show');
+        $this.children('i.fa').removeClass('fa-eye-slash').addClass('fa-eye');
+      } else {
+        $this.attr('data-visible', '1');
+        $this.children('span.text').text('Hide');
+        $this.children('i.fa').removeClass('fa-eye').addClass('fa-eye-slash');
+      }
+    });
   });
 
-  $('#js-toggle-code').click(function () {
-    $('#js-area-code').slideToggle(500, 'linear');
-    const $this = $(this);
-    if ($this.attr('data-visible') == '1') {
-      $this.attr('data-visible', '0');
-      $this.children('span.text').text('Show');
-      $this.children('i.fa').removeClass('fa-eye-slash').addClass('fa-eye');
-    } else {
-      $this.attr('data-visible', '1');
-      $this.children('span.text').text('Hide');
-      $this.children('i.fa').removeClass('fa-eye').addClass('fa-eye-slash');
-    }
-  });
 })();
